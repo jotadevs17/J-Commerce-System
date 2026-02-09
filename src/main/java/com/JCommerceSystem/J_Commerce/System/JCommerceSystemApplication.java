3412e86247072b7f1bd3c4012749acc2a7398a77
mr-robot <joao_flap@hotmail.com>
2026-02-09 16:33:50 -0300

    projeto criado

---
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/JCommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.JCommerceSystem.J_Commerce.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCommerceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JCommerceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JCommerceSystem/J_Commerce/System/JCommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JCommerceSystem.J_Commerce.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
