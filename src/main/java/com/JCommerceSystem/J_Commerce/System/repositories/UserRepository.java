811b2e29f41c8e89b2ff6e19d4b7fbff994adfcb
mr-robot <joao_flap@hotmail.com>
2026-02-10 18:55:38 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.JCommerceSystem.J_Commerce.System.repositories;
+
+import com.JCommerceSystem.J_Commerce.System.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
