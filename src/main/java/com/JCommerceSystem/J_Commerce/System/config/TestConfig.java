e6268c8595ff7fea23c9bfe86e7102f1e619aa4a
mr-robot <joao_flap@hotmail.com>
2026-02-21 12:28:29 -0300

    Category

--- src/main/java/com/JCommerceSystem/J_Commerce/System/config/TestConfig.java
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.JCommerceSystem.J_Commerce.System.config;
 
+import com.JCommerceSystem.J_Commerce.System.entities.Category;
 import com.JCommerceSystem.J_Commerce.System.entities.Order;
 import com.JCommerceSystem.J_Commerce.System.entities.User;
 import com.JCommerceSystem.J_Commerce.System.entities.enums.OrderStatus;
+import com.JCommerceSystem.J_Commerce.System.repositories.CategoryRepository;
 import com.JCommerceSystem.J_Commerce.System.repositories.OrderRepository;
 import com.JCommerceSystem.J_Commerce.System.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Jota", "jota@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Bruna Medeiros", "bruna@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.JCommerceSystem.J_Commerce.System.repositories;
+
+import com.JCommerceSystem.J_Commerce.System.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
+
