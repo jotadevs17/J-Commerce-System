99732c77e36d4b291543805b8ef41157429ee8cb
mr-robot <joao_flap@hotmail.com>
2026-03-09 11:59:29 -0300

    Exception handling - findById

---
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.JCommerceSystem.J_Commerce.System.resources.exceptions;
+
+import com.JCommerceSystem.J_Commerce.System.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/JCommerceSystem/J_Commerce/System/services/UserService.java
+++ src/main/java/com/JCommerceSystem/J_Commerce/System/services/UserService.java
@@ -2,6 +2,7 @@ package com.JCommerceSystem.J_Commerce.System.services;
 
 import com.JCommerceSystem.J_Commerce.System.entities.User;
 import com.JCommerceSystem.J_Commerce.System.repositories.UserRepository;
+import com.JCommerceSystem.J_Commerce.System.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
